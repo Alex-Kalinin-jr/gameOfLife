1a1b6a977b98cf31e58a83c968870474d744effd
Alex-Kalinin-jr <kalinakrasnaya1990@gmail.com>
2022-04-20 23:13:51 +0700

    it works

--- game.c
+++ game.c
@@ -9,6 +9,8 @@
 void initMatr(char **matr, char *arr);
 void output(char **matr);
 int input(char **matr, FILE *img);
+void drawing(char ***initMatr, char ***finalMatr);
+int rules(char **matr, int i, int j);
 
 int main(void) {
     system("clear");
@@ -28,7 +30,12 @@ int main(void) {
     initMatr(matr, arr);
     initMatr(cloneMatr, cloneArr);
     input(matr, img);
-    output(matr);
+
+    for (int i = 0; i < 100; i++) {
+        drawing(&matr, &cloneMatr);
+        output(matr);
+        system("sleep 0.5");
+    }
 
 
     fclose(img);
@@ -67,6 +74,52 @@ int input(char **matr, FILE *img) {
                 matr[i][j] = ' ';
             }
         }
+    }
+
+    return res;
+}
+
+void drawing(char ***initMatr, char ***finalMatr) {
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            if (rules(*initMatr, i, j) == 0) {
+                (*finalMatr)[i][j] = '*';
+            } else {
+                (*finalMatr)[i][j] = ' ';
+            }
+        }
+    }
+    *initMatr =  *finalMatr;
+}
+
+int rules(char **matr, int i, int j) {
+    int res, k, p;
+    int alive = 0;
+    for (int m = i - 1; m - i < 2; m++) {
+        for (int n = j - 1; n - j < 2; n++) {
+            k = m;
+            p = n;
+            if (m == -1)
+                k = HEIGHT - 1;
+            if (m == HEIGHT)
+                k = 0;
+            if (n == -1)
+                p = WIDTH - 1;
+            if (n == WIDTH)
+                p = 0;
+            if (!(m == i && n == j)) {
+                if (matr[k][p] == '*')
+                    alive++;
+            }
+        }
+    }
+
+    if (matr[i][j] == '*' && (alive == 2 || alive == 3)) {
+        res = 0;
+    } else if (matr[i][j] == ' ' && alive == 3) {
+        res = 0;
+    } else {
+        res = 1;
     }
 
     return res;
